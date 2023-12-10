51a46a458332d59f7dd8b44208cb65b93255165e
Hasan Imam <hasanjimam@gmail.com>
2023-12-10 21:28:17 +0900

    update the orders route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,8 +3,8 @@ import { UserController } from './user.controller';
 
 const router = express.Router();
 
-router.post('/create-user', UserController.createUser);
-router.get('/all-users', UserController.getAllUsers);
+router.post('/', UserController.createUser);
+router.get('/', UserController.getAllUsers);
 router.get('/:userId', UserController.getSingleUser);
 router.put('/:userId', UserController.updateUser);
 router.put('/:userId/orders', UserController.createOrders);
